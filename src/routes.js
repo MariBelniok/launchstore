eecda27465479be0a3b464e32f08c4a4ba6dcca8
Marianna Belniok <mariannabelniok@outlook.com>
2021-01-09 13:34:35 -0300

    search page working

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,16 @@ const express = require('express')
 const routes = express.Router()
 const multer = require('./app/middlewares/multer')
 const productController = require('./app/controllers/productController')
+const homeController = require('./app/controllers/homeController')
+const searchController = require('./app/controllers/searchController')
 
-routes.get('/', (req, res) => {
-    return res.render('layout.njk')
-})
+//HOME
+routes.get('/', homeController.index)
 
+//SEARCH 
+routes.get('/products/search', searchController.index)
+
+// PRODUCTS
 routes.get('/products/create', productController.create)
 routes.get('/products/:id', productController.show)
 routes.get('/products/:id/edit', productController.edit)
@@ -15,9 +20,11 @@ routes.post('/products', multer.array("photos", 6), productController.post)
 routes.put('/products', multer.array("photos", 6), productController.put)
 routes.delete('/products', productController.delete)
 
+//ALIAS
 routes.get('/ads/create', (req, res) => {
     return res.redirect('/products/create')
 })
+
 
 
 
